f17e38a1ad72751921d555d0ace8ee05775e1e57
Bernard Klatka <bklatka@evolution.com>
2023-08-08 08:25:54 +0200

    feat: some visual improvements

--- ui/src/features/weekSelector/weekSelector.tsx
+++ ui/src/features/weekSelector/weekSelector.tsx
@@ -1,8 +1,8 @@
-import { Button, Title1, Title3 } from "@fluentui/react-components";
+import { Body1, Body1Stronger, Button, Title1, Title2, Title3 } from "@fluentui/react-components";
 import { ArrowLeft16Regular, ArrowRight16Regular } from "@fluentui/react-icons";
 import { formatDate } from "../../utils/formatDate";
 import { endOfWeek, startOfWeek } from "date-fns";
-
+import styles from '../mainBoard/styles.module.scss'
 
 interface WeekSelectorProps {
     currentWeek: Date;
@@ -15,9 +15,9 @@ export const WeekSelector = ({ currentWeek, onNextWeek, onPrevWeek }: WeekSelect
     const beginningOfWeek = startOfWeek(currentWeek, { weekStartsOn: 1 });
     const endingOfWeek = endOfWeek(currentWeek, { weekStartsOn: 1 });
 
-    return <nav>
+    return <nav className={styles.weekSelector}>
         <Button onClick={onPrevWeek} icon={<ArrowLeft16Regular />} />
-        <Title3>{formatDate(beginningOfWeek)} - {formatDate(endingOfWeek)}</Title3>
+        <Title3>{formatDate(beginningOfWeek)} to {formatDate(endingOfWeek)}</Title3>
         <Button onClick={onNextWeek} icon={<ArrowRight16Regular />} />
     </nav>
 }
--- ui/src/utils/formatDate.ts
+++ ui/src/utils/formatDate.ts
@@ -2,7 +2,7 @@ import format from 'date-fns/format'
 
 export function formatDate(date: Date): string {
 
-    return format(new Date(date), "yyyy-MM-dd")
+    return format(new Date(date), "dd-MM-yyyy")
 }
 
 export function formatTime(date: Date): string {
